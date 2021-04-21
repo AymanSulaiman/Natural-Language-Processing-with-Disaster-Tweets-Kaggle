789c14468a896e7f4cdad232ce1e5f0013cd0b84
AymanSulaiman <saymansulaiman@gmail.com>
2021-04-20 19:09:44 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Kaggle-competition-involving-nlp',
+    author='Data-Slooth',
+    license='MIT',
+)
